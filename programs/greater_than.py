4dc4953f994b58287d1ce8e0e3f89e17d6993878
Naitik Singhal <81169690+naitiklovestech@users.noreply.github.com>
2024-07-03 14:06:27 +0530

    Update greater_than.py
    Updated the file for Hacker House Goa initial task

--- programs/greater_than.py
+++ programs/greater_than.py
@@ -1,14 +1,24 @@
 from nada_dsl import *
 
-
 def nada_main():
+    # Define the parties involved in the computation
     party1 = Party(name="Party1")
     party2 = Party(name="Party2")
+    
+    # Define secret inputs from both parties
     A = SecretInteger(Input(name="A", party=party1))
     B = SecretInteger(Input(name="B", party=party2))
     C = SecretInteger(Input(name="C", party=party1))
     D = SecretInteger(Input(name="D", party=party2))
-
+    
+    # Perform the computation
     result = A * B + C > B * D
-
+    
+    # Output the result to a specified party
     return [Output(result, "my_output", party1)]
+
+# Execute the NADA program
+if __name__ == "__main__":
+    result = nada_main()
+    for output in result:
+        print(output)
